ded7699247c33a0bfaab6d2beb972c062a2b0560
Sylvain <sysyl17@gmail.com>
2021-05-17 17:21:38 +0200

    first commit

---
+++ src/main/java/fr/univ_poitiers/dptinfo/aaw/mybankweb/MyBankWebApplication.java
@@ -0,0 +1,13 @@
+package fr.univ_poitiers.dptinfo.aaw.mybankweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBankWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBankWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/univ_poitiers/dptinfo/aaw/mybankweb/MyBankWebApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.univ_poitiers.dptinfo.aaw.mybankweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBankWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
